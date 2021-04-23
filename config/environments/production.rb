b1ca5fd6d98018ff2e95f59a20e8ec088314495b
masafumikazama <masa@codecabin.com.au>
2021-04-23 10:32:43 +1000

    setup mailer prodoction file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,20 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.default_url_options = { host: 'https://gentle-lake-69576.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'masanomaildesu@gmail.com', # my email
+    password:             'kmasafumi831', # email password
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
